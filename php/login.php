bf12baedc425561c0200a3343a89e0dae95fbe4b
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-07-25 03:33:49 +0000

    Arreglé nombres que estaban mal escritos y rutas mal definidas

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php
 // Conectar a la base de datos
-$conexion = new mysqli("localhost", "root", "", "cooperativa");
+$conexion = new mysqli("localhost", "root", "", "Cooperativa");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
@@ -10,7 +10,7 @@ $emailIngresado = $_POST['email'];
 $claveIngresada = $_POST['password'];
 
 // Buscar usuario por email
-$sql = "SELECT Password_hash FROM PEROSNA WHERE Email = ?"; //El signo de interogación sirve para indicar que se va a utilizar un parámetro en la consulta
+$sql = "SELECT Password_hash FROM PERSONA WHERE Email = ?"; //El signo de interogación sirve para indicar que se va a utilizar un parámetro en la consulta
 $stmt = $conexion->prepare($sql); //La base de datos prepara la consulta sql
 $stmt->bind_param("s", $emailIngresado); //Se ingresan el o los paramentros necesarios, en este caso un solo string, represnetado por la letra "s"
 $stmt->execute();
@@ -31,12 +31,12 @@ if ($resultado->num_rows === 1) { //Condicón que se haya encontrado una y solo 
         exit;
     } else {
         // Contraseña incorrecta
-        header("Location: index.php?error=invalido");
+        header("Location: ../Landing Page/inicio_sesion.php?error=invalido");
         exit;
     }
 } else {
     // Usuario no encontrado
-    header("Location: index.php?error=invalido");
+    header("Location: ../Landing Page/inicio_sesion.php?error=invalido");
     exit;
 }
 
