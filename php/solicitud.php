426898715829764b00dd6b48c489f1077894a0ca
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-07-25 03:35:21 +0000

    Terminé la API de solicitudes, la cual va a recibir si la solicitud fue aceptada o no.

--- php/solicitud.php
+++ php/solicitud.php
@@ -4,11 +4,12 @@
 //El backoffice debe contener estas lineas:
 
 //Si se oprimió el botón "aceptar"
-//$id = $tupla['ID'] -> Para seleccionar el ID de esa tupla con ese botón
-//header("Location: solicitud.php?solicitud=aprobada?ID=$id");
+//$id = $tupla['ID_Solicitud'] -> Para seleccionar el ID de esa tupla con ese botón
+//header("Location: solicitud.php?solicitud=aprobada?ID=" . $id"); ->Para redirigir a la página de solicitude el id del socio aceptado
 
 //Si se oprimió el botón "rechazar"
 //header("Location: solicitud.php?solicitud=rechazada");
+
 
 // Conexión a la base de datos
 $conexion = new mysqli("localhost", "root", "", "Cooperativa");
@@ -17,14 +18,40 @@ if ($conexion->connect_error) {
 }
 
 if (isset($_GET['solicitud']) && $_GET['solicitud'] == 'aprobada') {
-    // Insertar al nuevo socio
-    $ID = isset($_GET['registro']);
+    
+    // Extraer datos de la solicitud aprobada
+    
+    $ID = $_GET["ID"];
     $sql_extraer = "SELECT * FROM SOLICITUD WHERE ID = $ID";
     $fila_solicitud = $conexion->query($sql_extraer);
-    $nombre = 
-    $sql_insertar = "INSERT INTO SOCIO (nombre, , clave) VALUES (?, ?, ?)";
-    $stmt = $conexion->prepare($sql_insertar);
-    $stmt->bind_param("sss", $nombre, $email, $claveHasheada);
+    $fila_solicitud = $fila_solicitud->fetch_assoc();
+    $ci = $fila_solicitud["CI"];
+    $password = $fila_solicitud["Password_hash"];
+    $nombre = $fila_solicitud["Nombre"];
+    $apellido = $fila_solicitud["Apellido"];
+    $email = $fila_solicitud["Email"];
+    $telefono = $fila_solicitud["Nro_Telefono"];
+
+    // Insertar los datos en la tabla PERSONA
+    
+    $sql_insertar_persona = "INSERT INTO PERSONA (CI, Nombre, Apellido, Email, ID_Unidad_Habitacional, Password_hash) VALUES ($ci, $nombre, $apellido, $email, 1, $password)"; // Hay que analizar como se va a asignar la vivienda
+    $stmt = $conexion->prepare($sql_insertar_socio);
+    $stmt->execute();
+
+    // Insertar los datos en la tabla SOCIO
+
+    $sql_insertar_socio = "INSERT INTO SOCIO (CI) VALUES ($ci)";
+    $stmt = $conexion->prepare($sql_insertar_socio);
+    $stmt->execute();
+
+    $id_socio = $conexion->lastInsertId();
+
+    // Insertar los datos en la tabla TELEFONO
+
+    $sql_insertar_telefono = "INSERT INTO TELEFONO (Nro_Telefono, ID_Socio) VALUES ($telefono, $id_socio)";
+    $stmt = $conexion->prepare($sql_insertar_telefono);
+    $stmt->execute();
+
 }
 
 
