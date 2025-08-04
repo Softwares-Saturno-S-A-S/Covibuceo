ed31c400a761727ddd15038add409c0cb11b5501
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-08-04 21:03:37 +0000

    Creé un archivo php para mostrar el mensaje de solcitud enviada

---
+++ Landing Page/index_aceptado.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COVIBUCEO</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+    <style>
+        section{
+            width: 60vw;
+            max-width: 1000px;
+        }    
+    </style>
+</head>
+<body>
+    <div class="classic">
+        <h2>Solicitud enviada</h2>
+        <p>Le informamos que su solicititud para asociarse a COVIBUCEO fue enviada con éxito. Usted recibirá un correo electrónico a: {email} cuando gestionemos su solicitud.</p> <!--La idea es que esta linea muestre el correo electrónico enviado para la solicitud, el cual extraerá de la API -->
+        <p>En caso de ser aceptado le enviaremos el procedimiento a seguir para realizar su aporte inicial.</p>
+    <div>
+</body>
+</html>
