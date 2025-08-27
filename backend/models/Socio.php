ebd5b84492b0ccc526d4488ce8af6327bc66eda6
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-08-27 14:14:29 -0300

    Agregué un modelo para la clase teléfono y no mezclar la función en el modelo del socio

--- backend/models/Socio.php
+++ backend/models/Socio.php
@@ -9,7 +9,7 @@
         $this->conn = Database::getConnection();
     }
         public function add($input){
-            $stmt = $this->conn->prepare("SELECT * FROM " . $this->table_name . " WHERE CI = ? OR Email = ?"); // Consulta para verificar si ese usuario ya existe
+            $stmt = $this->conn->prepare("SELECT * FROM " . $this->table_name . " WHERE CI = ? OR Email = ? AND Estado_Socio = 'Aprobado'"); // Consulta para verificar si ese usuario ya existe
             $stmt->execute([ // Ejecuta la consulta con los parámetros proporcionados
             $input['ci'], 
             $input['email']
@@ -34,35 +34,27 @@
 
         // Insertar los teléfonos aparte
         $socio_id = $this->conn->lastInsertId(); // Obtiene el ID del socio recién insertado
+        $telefonos = $input['telefono'];
 
-        $this->insertarTelefonos($socio_id, $input['telefono']);
+        require_once 'Telefono.php'; // Incluye el modelo Telefono
 
-        private function insertarTelefonos($socio_id, $telefono_string) {
-        $telefonos = array_map('trim', explode(',', $telefono_string)); // Convierte la cadena en un array separando cada indice por comas y eliminando espacios en blanco
-    
-        $sql = "INSERT INTO TELEFONO (ID_Socio, Nro_Telefono) VALUES (?, ?)";
-        $stmt = $this->conn->prepare($sql);
-    
-    foreach ($telefonos as $telefono) { // Recorre el array e inserta cada teléfono
-        if (!empty($telefono)) {
-            $stmt->execute([
-                $socio_id,
-                $telefono
-            ]);
-        }
+        $telefono = new Telefono();
+        $resultado = $telefono->add($socio_id, $input['telefono']);
 
-            if ($insert) { // Si la inserción fue exitosa
+            if ($insert) { // Si la inserción fue exitosa devuelve el status 201 y un mensaje de éxito
                 return [
                     'status' => 201,
                     'mensaje' => '<h2>Solicitud Enviada</h2>
-                    <p class="p-left spaced">Le informamos que su solicitud para asociarse a <b>COVIBUCEO</b> fue enviada con éxito. Usted recibirá un correo electrónico a: ' . '<div class="link">' . $insert.['email'] . '</div>' . ' cuando gestionemos el estado de su solicitud.</p>
+                    <p class="p-left spaced">Le informamos que su solicitud para asociarse a <b>COVIBUCEO</b> fue enviada con éxito. Usted recibirá un correo electrónico a: ' . '<div class="link">' . $input['email'] . '</div>' . ' cuando gestionemos el estado de su solicitud.</p>
                     <p class="p-left spaced">En caso de aprobar su solicitud le enviaremos los datos para realizar su aporte inicial, y la cuota mensual de su vivienda más los gastos comunes.</p>
                     <button class="button-longsize light-green">Aceptar</button>'
                 ];
+            } else { // Si la inserccion no fue exitosa, por error de conexión, devuelve el status 500 y un mensaje de error
+                return [
+                    'status' => 500,
+                    'error' => 'Error al registrar solicitud: ' . $stmt->errorInfo()[2] 
+                ];
             }
         }
-        }
-    }
-}
     }
 ?>
---
+++ backend/models/Telefono.php
@@ -0,0 +1,29 @@
+<?php
+    require_once 'Database.php'; // Llama el archivo con la conexión a la base de datos
+
+    class Telefono {
+        private $conn;
+        private $table_name = "TELEFONO";
+
+        public function __construct() { // Constructor que inicializa la conexión a la base de datos
+        $this->conn = Database::getConnection();
+    }
+
+    public function add($socio_id, $telefono_string) {
+        $telefonos = array_map('trim', explode(',', $telefono_string)); // Convierte la cadena en un array separando cada indice por comas y eliminando espacios en blanco
+    
+        $sql = "INSERT INTO TELEFONO (ID_Socio, Nro_Telefono) VALUES (?, ?)";
+        $stmt = $this->conn->prepare($sql);
+    
+    foreach ($telefonos as $telefono) { // Recorre el array e inserta cada teléfono
+        if (!empty($telefono)) {
+            $stmt->execute([
+                $socio_id,
+                $telefono
+            ]);
+        }
+}
+    }
+}
+
+?>
