f38d28f0142e702c329a421ef01568adce8fa06b
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-09-04 20:21:04 +0000

    Moví la carpeta "backoffice" dentro de la carpeta "frontend"

--- frontend/backoffice/index.php
+++ frontend/backoffice/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COVIBUCEO - Home Administración</title>
-    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../../css/estilos.css">
     <style>
         button {
             margin-left: 25px;
