86a86d58a4db8ac6928afed8b6b725bfb38684b3
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-09-05 19:35:33 -0300

    Variables de entorno para la conexióna Base de Datos

--- source/backend/config/Database.php
+++ source/backend/config/Database.php
@@ -1,19 +1,19 @@
 <?php
 class Database
 {
-    private $host = 'localhost';
-    private $dbname = 'cooperativa';
-    private $username = 'root';
-    private $password = '';
+    // Establecer las variables de conexión a la base de datos desde variables de entorno y proveer valores por defecto
+    private $host = $_ENV['DB_HOST'] ?? 'localhost';
+    private $dbname = $_ENV['DB_NAME'] ?? 'default';
+    private $username = $_ENV['DB_USER'] ?? 'root';
+    private $password = $_ENV['DB_PASS'] ?? '';
+
     private $connection = null;
    
     public function getConnection() {
          if ($this->connection === null) {
             try {
                 $this->connection = new PDO(
-                    "mysql:host=" . $this->host . ";dbname=" . $this->dbname, 
-                    $this->username, 
-                    $this->password
+                    "mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password
                 );
             
             $this->connection->exec("set names utf8");
