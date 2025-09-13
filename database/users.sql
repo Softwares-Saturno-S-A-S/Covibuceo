d8e71dbae94124f7b824f75bd656b53659124bd3
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-09-13 00:47:49 -0300

    Sentencias de permisos backoffice y socios

--- database/users.sql
+++ database/users.sql
@@ -1,5 +1,14 @@
-CREATE USER  IF NOT EXISTS 'web_user'@'%' IDENTIFIED BY 'web_password';
+CREATE USER  IF NOT EXISTS 'web_user_socio'@'%' IDENTIFIED BY 'socio_password';
 
-GRANT ALL PRIVILEGES ON cooperativa.* TO 'web_user'@'%';
+GRANT SELECT, UPDATE, INSERT ON cooperativa.SOCIO TO 'web_user_socio'@'%';
+GRANT SELECT, UPDATE, INSERT, DELETE ON cooperativa.TELEFONO TO 'web_user_socio'@'%';
+GRANT SELECT, INSERT ON cooperativa.COMPROBANTE TO 'web_user_socio'@'%';
+GRANT SELECT ON cooperativa.PAGO TO 'web_user_socio'@'%';
+GRANT SELECT ON cooperativa.UNIDAD_HABITACIONAL TO 'web_user_socio'@'%';
+GRANT SELECT, INSERT, UPDATE ON cooperativa.JORNADA_LABORAL TO 'web_user_socio'@'%';    
+
+CREATE USER IF NOT EXISTS 'web_user_admin'@'%' IDENTIFIED BY 'admin_password';
+
+GRANT ALL PRIVILEGES ON cooperativa.* TO 'web_user_admin'@'%';
 
 FLUSH PRIVILEGES;
