5ed2d4c0bc238f87c2de5f7e2a951def6bdd776e
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-09-15 13:13:36 -0300

    ips nuevamente corregidas

--- database/users.sql
+++ database/users.sql
@@ -1,18 +1,18 @@
-CREATE USER  IF NOT EXISTS 'web_user_socio'@'172.18.0.4' IDENTIFIED BY 'socio_password';
+CREATE USER  IF NOT EXISTS 'web_user_socio'@'%' IDENTIFIED BY 'socio_password';
 
-GRANT SELECT, UPDATE, INSERT ON cooperativa.SOCIO TO 'web_user_socio'@'172.18.0.4';
-GRANT SELECT, UPDATE, INSERT, DELETE ON cooperativa.TELEFONO TO 'web_user_socio'@'172.18.0.4';
-GRANT SELECT, INSERT ON cooperativa.COMPROBANTE TO 'web_user_socio'@'172.18.0.4';
-GRANT SELECT ON cooperativa.PAGO TO 'web_user_socio'@'172.18.0.4';
-GRANT SELECT ON cooperativa.UNIDAD_HABITACIONAL TO 'web_user_socio'@'172.18.0.4';
-GRANT SELECT, INSERT, UPDATE ON cooperativa.JORNADA_LABORAL TO 'web_user_socio'@'172.18.0.4';    
+GRANT SELECT, UPDATE, INSERT ON cooperativa.SOCIO TO 'web_user_socio'@'%';
+GRANT SELECT, UPDATE, INSERT, DELETE ON cooperativa.TELEFONO TO 'web_user_socio'@'%';
+GRANT SELECT, INSERT ON cooperativa.COMPROBANTE TO 'web_user_socio'@'%';
+GRANT SELECT ON cooperativa.PAGO TO 'web_user_socio'@'%';
+GRANT SELECT ON cooperativa.UNIDAD_HABITACIONAL TO 'web_user_socio'@'%';
+GRANT SELECT, INSERT, UPDATE ON cooperativa.JORNADA_LABORAL TO 'web_user_socio'@'%';    
 
-CREATE USER IF NOT EXISTS 'web_user_admin'@'172.18.0.4' IDENTIFIED BY 'admin_password';
+CREATE USER IF NOT EXISTS 'web_user_admin'@'%' IDENTIFIED BY 'admin_password';
 
-GRANT ALL PRIVILEGES ON cooperativa.* TO 'web_user_admin'@'172.18.0.4';
+GRANT ALL PRIVILEGES ON cooperativa.* TO 'web_user_admin'@'%';
 
-CREATE USER IF NOT EXISTS 'backup_user'@'172.18.0.3' IDENTIFIED BY 'backup_password';
+CREATE USER IF NOT EXISTS 'backup_user'@'%' IDENTIFIED BY 'backup_password';
 
-GRANT SELECT ON cooperativa.* TO 'backup_user'@'172.18.0.3';
+GRANT SELECT ON cooperativa.* TO 'backup_user'@'%';
 
 FLUSH PRIVILEGES;
