5e374924d54f6581d1a9f33cf2f178990e2f246e
Sofia-Morales101 <smorales@estudiante.ceibal.edu.uy>
2025-09-15 15:09:08 -0300

    lógica de parametros arreglada

--- source/frontend/js/aceptado.js
+++ source/frontend/js/aceptado.js
@@ -6,9 +6,9 @@
 
         const contenedor = document.getElementById('mensaje-container');
         const urlParams = new URLSearchParams(window.location.search); // Busca los parámetros de la URL
-        const mensajeCodificado = urlParams.get('mensaje'); // Obtiene el mensaje codificado encontrado en la URL
+        const mensaje = urlParams.get('mensaje'); // Obtiene el mensaje codificado encontrado en la URL
 
-        if (mensajeCodificado) {
+        if (mensaje === 'aceptado') {
             try {
                 // Construye mensaje HTML
                 const mensajeHTML = `
@@ -16,7 +16,7 @@
                         <h2>Solicitud Enviada</h2>
                         <p class="left spaced">Le informamos que su solicitud para asociarse a <b>COVIBUCEO</b> fue enviada con éxito. Usted recibirá un correo electrónico a: <span class="link">${resultado.email}</span> cuando gestionemos el estado de su solicitud.</p>
                         <p class="left spaced">En caso de aprobar su solicitud le enviaremos los datos para realizar su aporte inicial. Una vez realice este aporte, se le asignara una vivienda en base a las necesidades solicitadas.</p>
-                        <button class="button-longsize light-green" type="button" onclick="close()">Aceptar</button>
+                        <button class="button-longsize light-green" type="button">Aceptar</button>
                     </div>
                 `;
             
